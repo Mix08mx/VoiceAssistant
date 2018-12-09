de3c21c218e4c0e536103480271506458fbd95a0
Mix08mx <45737678+Mix08mx@users.noreply.github.com>
2018-12-09 11:39:51 -0600

    Add files via upload

---
+++ cloudspeech_demo.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A demo of the Google CloudSpeech recognizer."""
+import argparse
+import locale
+import logging
+import paho.mqtt.publish as publish
+from aiy.voice import tts
+
+from aiy.board import Board, Led
+from aiy.cloudspeech import CloudSpeechClient
+
+
+def get_hints(language_code):
+    if language_code.startswith('en_'):
+        return ('enciende la luz',
+                'apaga la luz',
+               'parpadea',
+                'adios')
+    return None
+
+def locale_language():
+    language, _ = locale.getdefaultlocale()
+    return language
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description='Assistant service example.')
+    parser.add_argument('--language', default=locale_language())
+    args = parser.parse_args()
+
+    logging.info('Initializing for language %s...', args.language)
+    hints = get_hints(args.language)
+    client = CloudSpeechClient()
+    with Board() as board:
+        while True:
+            if hints:
+                logging.info('Say something, e.g. %s.' % ', '.join(hints))
+            else:
+                logging.info('Say something.')
+            text = client.recognize(language_code=args.language,
+                                    hint_phrases=hints)
+            if text is None:
+                logging.info('You said nothing.')
+                continue
+
+            logging.info('You said: "%s"' % text)
+            text = text.lower()
+            if 'enciende la luz' in text:
+                board.led.state = Led.ON
+                tts.say('Light powered on')
+            elif 'apaga la luz' in text:
+                board.led.state = Led.OFF
+            elif 'parpadea' in text:
+                board.led.state = Led.BLINK
+            elif 'hasta luego' in text:
+                break
+
+if __name__ == '__main__':
+    main()
